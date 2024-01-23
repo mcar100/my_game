cbf9de36db67b99234de9895742c7aaabc6d4d14
승섭kim <mcar100@naver.com>
2024-01-23 20:29:05 +0900

    Feat: 게임 시작 후 버튼별 입력 효과 추가

--- src/pages/GamePage/ArrowKeyGame/ArrowKeyCommand.tsx
+++ src/pages/GamePage/ArrowKeyGame/ArrowKeyCommand.tsx
@@ -1,20 +1,43 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { GAME_STATE } from "../../../utils/constants";
-
 import { RootState } from "../../../utils/redux/store";
 
+interface PressedState {
+  command1: boolean;
+  command2: boolean;
+  command3: boolean;
+}
+
 function ArrowKeyCommand() {
+  const initState: PressedState = {
+    command1: false,
+    command2: false,
+    command3: false,
+  };
+  const [isPressed, setIsPressed] = useState<PressedState>(initState);
   const gameMode = useSelector((state: RootState) => state.common.gameState);
   const commandList = useSelector(
     (state: RootState) => state.arrowGame.commandList
   );
-
+  const handleCommandBtnPressed = (e: KeyboardEvent) => {
+    if (e.key === commandList.command1) {
+      setIsPressed(() => ({ ...initState, command1: true }));
+    } else if (e.key === commandList.command2) {
+      setIsPressed(() => ({ ...initState, command2: true }));
+    } else if (e.key === commandList.command3) {
+      setIsPressed(() => ({ ...initState, command3: true }));
+    } else {
+      setIsPressed(() => ({ ...initState }));
+    }
+  };
   useEffect(() => {
     if (gameMode === GAME_STATE.PROCEEDING) {
       document.addEventListener("keydown", handleKeyDown);
+      document.addEventListener("keydown", handleCommandBtnPressed);
     } else {
       document.removeEventListener("keydown", handleKeyDown);
+      document.removeEventListener("keydown", handleCommandBtnPressed);
     }
 
     function handleKeyDown(e: KeyboardEvent): void {
@@ -29,23 +52,24 @@ function ArrowKeyCommand() {
     return () => {
       if (gameMode === GAME_STATE.PROCEEDING) {
         document.removeEventListener("keydown", handleKeyDown);
+        document.removeEventListener("keydown", handleCommandBtnPressed);
       }
     };
   }, [gameMode, commandList]);
 
   return (
     <div className="command-box">
-      <div className="command-button">
+      <div className={isPressed.command1 ? "active" : ""}>
         <button className="game-button" value={commandList.command1}>
           ◁
         </button>
       </div>
-      <div className="command-button">
+      <div className={isPressed.command2 ? "active" : ""}>
         <button className="game-button" value={commandList.command2}>
           △
         </button>
       </div>
-      <div className="command-button">
+      <div className={isPressed.command3 ? "active" : ""}>
         <button className="game-button" value={commandList.command3}>
           ▷
         </button>
